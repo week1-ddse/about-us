bbb1c354156a5e66b5195e277abaf61a3102c5f2
DannySuarez <suarezd10@gmail.com>
2019-04-23 13:25:39 -0700

    created the index.js file

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const dannySAnswer = document.getElementById('dannys-from');
+const dannyHAnswer = document.getElementById('dannyh-from');
+const susanAnswer = document.getElementById('susan-from');
+const eliAnswer = document.getElementById('eli-from');
+const teamAcronymAnswer = document.getElementById('team-acronym');
+const result = document.getElementById('quiz-results');
+
+console.log(dannySAnswer, dannyHAnswer, susanAnswer, eliAnswer, teamAcronymAnswer, result);
