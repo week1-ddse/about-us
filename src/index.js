83d3517136e129940533f880f8096337c62c4433
DannySuarez <suarezd10@gmail.com>
2019-04-23 13:32:19 -0700

    added event listener to button

--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ const susanAnswer = document.getElementById('susan-from');
 const eliAnswer = document.getElementById('eli-from');
 const teamAcronymAnswer = document.getElementById('team-acronym');
 const result = document.getElementById('quiz-results');
+const button = document.getElementById('submit');
+button.addEventListener('click', asset => {
 
-console.log(dannySAnswer, dannyHAnswer, susanAnswer, eliAnswer, teamAcronymAnswer, result);
+    console.log("you clicked a button")
+
+});
+console.log(dannySAnswer, dannyHAnswer, susanAnswer, eliAnswer, teamAcronymAnswer, result, button);
+
